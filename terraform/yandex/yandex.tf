a46b7a44679ee30a628792ec99dab1be91e28636
b0xmaks <m.3anin@gmail.com>
2022-05-05 18:07:00 +0300

    main

--- terraform/yandex/yandex.tf
+++ terraform/yandex/yandex.tf
@@ -13,3 +13,33 @@ provider "yandex" {
   folder_id = "default"
   zone      = "ru-central-b"
 }
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8fte6bebi857ortlja"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
